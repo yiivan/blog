9df753767f4d6d838522a7b5f7b8da0d5c45b95c
yiivan <yiivan@hotmail.com>
2016-06-29 16:00:18 -0700

    style home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
   def index
+    @post01 = Post.order(:id).last
+    @post02 = Post.order(:id).last(2).first
+    @post03 = Post.order(:id).last(3).first
   end
 
   def about
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,6 +40,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [300, 200]
   end
 
+  version :medium do
+    process :resize_to_fill => [1080, 720]
+  end
+
   version :large do
     process :resize_to_fit => [1200, 800]
   end
