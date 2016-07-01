9b242f5d656597b79351512dfe4e5da0be84b4e3
yiivan <yiivan@hotmail.com>
2016-07-01 10:45:58 -0700

    fix carousel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @post01 = Post.order(:id).last
-    @post02 = Post.order(:id).last(2).first
-    @post03 = Post.order(:id).last(3).first
+    @posts = Post.order(:id).last(3)
   end
 
   def about
